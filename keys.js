761c41075c837793cdbe1f1cf54fab5431f8a587
Armmand Alston <ArmmandAlston33@gmail.com>
2017-04-27 12:03:42 -0400

    Updated Code

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {  
+ consumer_key: 'ZndBiZfFe0ua9IGf1p7uAcIfM',   
+ consumer_secret:'7g28KliSeSOlAjHdVuRz7YWPRLAY0KMNRtsmE39W3DfJNJu9OW',   
+ access_token_key: '857606539565305856-N9906DhsfXuc30z63QRrBNDh18wcTxF',   
+ access_token_secret:'RbHDglvSDq6QonrvJcXWovDyxod22XDvzyP5TcgWasgFz', }
+      
